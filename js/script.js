7e95cd2f49f612a3a006232020173a13c146533e
IIODERII <oderrisi@gmail.com>
2023-11-02 14:10:32 +0100

    initialize project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+import { contactList } from "./data.js";
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      contacts: contactList,
+    };
+  },
+  methods: {},
+  computed: {},
+}).mount("#app");
