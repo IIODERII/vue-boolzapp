f5f8013397df23978c66386560c76542dffb7671
IIODERII <oderrisi@gmail.com>
2023-11-02 18:36:36 +0100

    added sendmessage function

--- js/script.js
+++ js/script.js
@@ -8,11 +8,25 @@ createApp({
       contacts: contactList,
       lastID: 8,
       activeContact: 1,
+      newMessage: "",
     };
   },
   methods: {
     onlyHour(date) {
       return date.slice(11, 16);
+    },
+
+    isActive(id) {
+      return id === this.activeContact ? true : false;
+    },
+
+    sendMessage() {
+      this.contacts[this.activeIndex].messages.push({
+        date: new Date().toLocaleString(),
+        message: this.newMessage,
+        status: "sent",
+      });
+      this.newMessage = "";
     },
   },
   computed: {
